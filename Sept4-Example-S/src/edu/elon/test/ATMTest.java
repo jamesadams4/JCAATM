6bfa1ab6aaa8f1abb5cf845e2b55392e71da923c
James Adams <jadams27@elon.edu>
2014-09-06 13:58:09 -0400

    Fixed Javadoc

--- Sept4-Example-S/src/edu/elon/test/ATMTest.java
+++ Sept4-Example-S/src/edu/elon/test/ATMTest.java
@@ -20,66 +20,66 @@ import org.junit.Test;
  *
  */
 public class ATMTest {
-	private ATM atm;
+  private ATM atm;
 
-	/**
-	 * Initializes an instance of ATM before each JUnit test case.
-	 * 
-	 * @throws Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-		atm = new ATM(10.0);
-	}
+  /**
+   * Initializes an instance of ATM before each JUnit test case.
+   * 
+   * @throws Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    atm = new ATM(10.0);
+  }
 
-	/**
-	 * Sets an instance of ATM to null after each JUnit test case.
-	 * 
-	 * @throws Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-		atm = null;
-	}
+  /**
+   * Sets an instance of ATM to null after each JUnit test case.
+   * 
+   * @throws Exception
+   */
+  @After
+  public void tearDown() throws Exception {
+    atm = null;
+  }
 
-	/**
-	 * JUnit test case to test the deposit method.
-	 * Test passes if IllegalArgumentException is thrown 
-	 * when depositing negative values.
-	 */
-	@Test(expected=IllegalArgumentException.class)
-	public void testDeposit() {
-		atm.deposit(-15.0);
-	}
+  /**
+   * JUnit test case to test the deposit method.
+   * Test passes if IllegalArgumentException is thrown 
+   * when depositing negative values.
+   */
+  @Test(expected=IllegalArgumentException.class)
+  public void testDeposit() {
+    atm.deposit(-15.0);
+  }
 
-	/**
-	 * JUnit test case to test the getBalance method.
-	 */
-	@Test
-	public void testGetBalance() {
-		double actual = atm.getBalance();
-		double expected = 10.0;
-		assertEquals(expected, actual, 0.1);
-	}
+  /**
+   * JUnit test case to test the getBalance method.
+   */
+  @Test
+  public void testGetBalance() {
+    double actual = atm.getBalance();
+    double expected = 10.0;
+    assertEquals(expected, actual, 0.1);
+  }
 
-	/**
-	 * JUnit test case to test the toString method.
-	 */
-	@Test
-	public void testToString() {
-		String actual = atm.toString();
-		String expected = "Amount balance is $10.00";
-		assertEquals(expected, actual);
-	}
+  /**
+   * JUnit test case to test the toString method.
+   */
+  @Test
+  public void testToString() {
+    String actual = atm.toString();
+    String expected = "Amount balance is $10.00";
+    assertEquals(expected, actual);
+  }
 
-	/**
-	 * JUnit test case to test the withdraw method.
-	 * Test passes if IllegalArgumentException is thrown 
-	 * when withdrawing negative values.
-	 */
-	@Test(expected=IllegalArgumentException.class)
-	public void testWithdraw() {
-		atm.withdraw(20.0);
-	}
+  /**
+   * JUnit test case to test the withdraw method.
+   * Test passes if IllegalArgumentException is thrown 
+   * when withdrawing negative values.
+   */
+  @Test(expected=IllegalArgumentException.class)
+  public void testWithdraw() {
+    atm.withdraw(20.0);
+  }
 
 }
